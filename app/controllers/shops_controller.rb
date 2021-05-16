bb3c43744a4add2dd715c84183659fbb30bb08b9
mauro <mauro_gs@hotmail.com>
2021-05-16 18:05:24 +0200

    Scaffold shop and database creation
    Only index and show

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,16 @@
+class ShopsController < ApplicationController
+  before_action :set_shop, only: :show
+
+  def index
+    @shops = Shop.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+end
