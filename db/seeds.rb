2d01831afc607b87a87fe90d2bb8188c33356cc2
mauro <mauro_gs@hotmail.com>
2021-05-16 19:18:23 +0200

    Fill database with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+shop_one = Shop.create(name: 'ikea')
+shop_two = Shop.create(name: 'carrefour')
+
+schedule_one = Schedule.create(
+  monday: false,
+  tuesday: true,
+  wednesday: true,
+  thursday: true,
+  friday: true,
+  saturday: true,
+  sunday: false,
+
+  monday_hours: nil,
+  tuesday_hours: ["10:00-12:00", "15:00-19:00"],
+  wednesday_hours: ["10:00-12:00", "15:00-19:00"],
+  thursday_hours: ["10:00-12:00", "15:00-19:00"],
+  friday_hours: ["10:00-12:00", "15:00-19:00"],
+  saturday_hours: ["10:00-14:00"],
+  sunday_hours: nil
+)
+shop_one.schedule = schedule_one
+
+schedule_two = Schedule.create(
+    monday: true,
+    tuesday: true,
+    wednesday: true,
+    thursday: true,
+    friday: true,
+    saturday: true,
+    sunday: false,
+
+    monday_hours: ["09:00-12:00", "15:00-19:00"],
+    tuesday_hours: ["09:00-12:00", "15:00-19:00"],
+    wednesday_hours: ["09:00-16:00"],
+    thursday_hours: ["09:00-12:00", "15:00-19:00"],
+    friday_hours: ["09:00-12:00", "15:00-19:00"],
+    saturday_hours: ["09:00-15:00"],
+    sunday_hours: nil
+  )
+shop_two.schedule = schedule_two
