13cf81705cfa11c41f8ff7cc63470312fbe691a7
mauro <mauro_gs@hotmail.com>
2021-05-16 20:43:02 +0200

    Add schedule validations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,46 @@
 class Schedule < ApplicationRecord
-  belongs_to :shop  
+  belongs_to :shop
+
+  validate :hours_should_be_unique,
+    :hours_should_not_be_overlapped,
+    :hours_should_not_be_empty    
+
+  def hours_should_be_unique
+    Date::DAYNAMES.each do |day|
+      hours_hand = hours(day)
+      next if hours_hand.nil? || hours_hand == hours_hand.uniq
+
+      errors.add(:hours, "Hours are duplicated")
+    end
+  end
+
+  def hours_should_not_be_overlapped
+    Date::DAYNAMES.each do |day|
+      hours_hand = hours(day)
+      next if hours_hand.nil? || hours_hand.length <= 1
+      next unless overlapped?(hours_hand.first, hours_hand.last)
+
+      errors.add(:hours, "Hours are overlapped")
+    end
+  end
+
+  def hours_should_not_be_empty
+    Date::DAYNAMES.each do |day|
+      hours_hand = hours(day)
+      next if hours_hand.nil? || hours_hand.all?(&:present?)
+
+      errors.add(:hours, "Hours are empty")
+    end
+  end
+
+  def overlapped?(range_one, range_two)
+    first = range_one.split('-')
+    last = range_two.split('-')
+
+    (first[0]..first[1]).overlaps?(last[0]..last[1])
+  end
+
+  def hours(day)
+    send(day.downcase + '_hours')
+  end
 end
---
+++ spec/models/schedule_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Schedule, type: :model do
+  let(:shop) { Shop.create(name: 'store') }
+
+  it 'should create a new schedule object' do 
+    schedule = Schedule.new
+
+    expect(schedule).to be_a Schedule
+  end
+
+  it 'should respond to his fields' do 
+    schedule = Schedule.new
+
+    expect(schedule).to respond_to(:monday)
+    expect(schedule).to respond_to(:monday_hours)
+    expect(schedule).to respond_to(:sunday)
+    expect(schedule).to respond_to(:sunday_hours)
+  end
+
+  it 'should not create a schedule without shop association' do 
+    schedule = Schedule.new(monday: false, tuesday: true)
+
+    expect(schedule.valid?).to be false
+    expect(schedule.errors.messages[:shop]).to include('must exist')
+  end
+
+  it 'should not create a schedule without shop association' do 
+    schedule = Schedule.new(shop_id: shop.id, monday: false, tuesday: true)
+
+    expect(schedule.valid?).to be true
+    expect(schedule.errors.messages[:shop]).to be_empty
+  end
+
+  it 'should not create hours with duplicated fields' do 
+    wrong_hours = ["10:00-12:00", "10:00-12:00"]
+    schedule = Schedule.new(shop_id: shop.id, monday: false, monday_hours: wrong_hours)
+
+    expect(schedule.valid?).to be false
+    expect(schedule.errors.messages[:hours]).to include("Hours are duplicated")
+  end
+
+  it 'should not create hours with overlapped hours' do 
+    wrong_hours = ["10:00-12:00", "12:00-15:00"]
+    schedule = Schedule.new(shop_id: shop.id, monday: false, monday_hours: wrong_hours)
+
+    expect(schedule.valid?).to be false
+    expect(schedule.errors.messages[:hours]).to include("Hours are overlapped")
+  end
+
+  it 'should not create hours with empty hours' do 
+    wrong_hours = [""]
+    schedule = Schedule.new(shop_id: shop.id, monday: false, monday_hours: wrong_hours)
+
+    expect(schedule.valid?).to be false
+    expect(schedule.errors.messages[:hours]).to include("Hours are empty")
+  end
+
+  it 'should create a schedule with valid hours' do 
+    hours = ["10:00-12:00", "15:00-20:00"]
+    schedule = Schedule.new(shop_id: shop.id, monday: false, monday_hours: hours)
+
+    expect(schedule.valid?).to be true
+    expect(schedule.errors.messages[:hours]).to be_empty
+    expect(schedule.errors.messages[:hours]).to be_blank
+  end
+end
